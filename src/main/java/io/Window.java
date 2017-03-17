04912c0ff658859dafea03eb8fa41a790cbb244b
GaëlCR <gaelcr@iMacGael.local>
2017-03-17 14:19:24 +0100

    [IRC] - Fenêtre avancée.

--- src/main/java/io/Window.java
+++ src/main/java/io/Window.java
@@ -1,21 +1,334 @@
 package io;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
+/*
+ * 
+ * Created on 10 mars, 2017
+ * version 1
+ *
+ */
 
-public class Window {
 
-	public Window() {
-		JFrame window = new JFrame();
-		
-		
-		JButton bouton =new JButton("clique");
-		window.add(bouton);
-		window.setTitle("IRC");
-		window.setSize(400, 500);
-		window.setLocationRelativeTo(null);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setVisible(true);
-	}
+import java.net.*;
+import java.io.*;
+import java.util.*;
 
+/**
+ * 
+ * @author Cnam
+ * 
+ */
+public class Window extends javax.swing.JFrame {
+	
+	
+    // Variables declaration - do not modify                     
+    private javax.swing.JTextArea chatTextArea;
+    private javax.swing.JButton connectButton;
+    private javax.swing.JButton disconnectButton;
+    private javax.swing.JTextArea inputTextArea;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JButton sendButton;
+    private javax.swing.JTextField usernameField;
+    private javax.swing.JTextArea usersList;
+    // End of variables declaration              
+	
+	
+//    String username, serverIP = "";
+//    int Port = 0;
+    Socket sock;
+    BufferedReader reader;
+    PrintWriter writer;
+    ArrayList<String> userList = new ArrayList();
+    Boolean isConnected = false;
+
+
+    /** Creates new form Chat */
+    public Window() {
+        initComponents();
+    }
+    
+    
+    /*	Permet de gérer les différentes connexions	 */
+
+    public class IncomingReader implements Runnable{
+
+        public void run() {
+            String[] data;
+            String stream, done = "Terminer", connect = "Connecter", disconnect = "Deconnecter", chat = "Chat";
+
+            try {
+                while ((stream = reader.readLine()) != null) {
+
+                    data = stream.split(":");
+
+                     if (data[2].equals(chat)) {
+
+                        chatTextArea.append(data[0] + ": " + data[1] + "\n");
+                        chatTextArea.setCaretPosition(chatTextArea.getDocument().getLength());
+
+                    } else if (data[2].equals(connect)){
+
+                        chatTextArea.removeAll();
+                        userAdd(data[0]);
+
+                    } else if (data[2].equals(disconnect)) {
+
+
+                        userRemove(data[0]);
+
+                    } else if (data[2].equals(done)) {
+
+
+                        usersList.setText("");
+                        writeUsers();
+                        userList.clear();
+
+                    }
+                 
+                }
+           }catch(Exception ex) {
+           }
+        }
+    }
+    
+
+    /*	 Appel du Thread	*/
+    public void ListenThread() {
+         Thread IncomingReader = new Thread(new IncomingReader());
+         IncomingReader.start();
+    }
+
+    
+    
+    
+    /*	 Méthodes User	*/
+    public void userAdd(String data) {
+         userList.add(data);
+
+     }
+
+    public void userRemove(String data) {
+         chatTextArea.append(data + " s'"
+         		+ "est déconnecté.\n");
+
+     }
+
+    public void writeUsers() {
+         String[] tempList = new String[(userList.size())];
+         userList.toArray(tempList);
+         for (String token:tempList) {
+
+             usersList.append(token + "\n");
+
+         }
+
+     }
+
+//    public void sendDisconnect() {
+//
+//       String bye = (username + ": :Déconnecté");
+//        try{
+//            writer.println(bye); // Sends server the disconnect signal.
+//            writer.flush(); // flushes the buffer
+//        } catch (Exception e) {
+//            chatTextArea.append("Impossible d'envoyer le message déconnecté.\n");
+//        }
+//
+//      }
+
+    public void Disconnect() {
+
+        try {
+               chatTextArea.append("Déconnecté.\n");
+               sock.close();
+        } catch(Exception ex) {
+               chatTextArea.append("Échec de la déconnexion. \n");
+        }
+        isConnected = false;
+        usernameField.setEditable(true);
+        usersList.setText("");
+
+      }
+
+    
+    /*	 Initialisation du visuel  	*/
+    
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        inputTextArea = new javax.swing.JTextArea();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        chatTextArea = new javax.swing.JTextArea();
+        jLabel1 = new javax.swing.JLabel();
+        usernameField = new javax.swing.JTextField();
+        connectButton = new javax.swing.JButton();
+        disconnectButton = new javax.swing.JButton();
+        sendButton = new javax.swing.JButton();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        usersList = new javax.swing.JTextArea();
+        jLabel2 = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Chat IRC Cnam");
+
+        inputTextArea.setColumns(20);
+        inputTextArea.setLineWrap(true);
+        inputTextArea.setRows(5);
+        jScrollPane1.setViewportView(inputTextArea);
+
+        chatTextArea.setColumns(20);
+        chatTextArea.setEditable(false);
+        chatTextArea.setFont(new java.awt.Font("Times New Roman", 0, 12));
+        chatTextArea.setLineWrap(true);
+        chatTextArea.setRows(5);
+        jScrollPane2.setViewportView(chatTextArea);
+
+        jLabel1.setText("Username:");
+
+        connectButton.setText("Connexion");
+        connectButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                connectButtonActionPerformed(evt);
+            }
+        });
+
+        disconnectButton.setText("Déconnexion");
+        disconnectButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                disconnectButtonActionPerformed(evt);
+            }
+        });
+
+        sendButton.setText("Envoyer");
+        sendButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sendButtonActionPerformed(evt);
+            }
+        });
+
+        usersList.setEditable(false);
+        usersList.setColumns(20);
+        usersList.setRows(5);
+        jScrollPane3.setViewportView(usersList);
+
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Online Users");
+
+        setJMenuBar(jMenuBar1);
+        
+        /*	 Instanciation de la fenetre et ajout des composants 	*/
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(4, 4, 4)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 336, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(sendButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 419, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(usernameField, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(connectButton)
+                        .addGap(18, 18, 18)
+                        .addComponent(disconnectButton)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jScrollPane3))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(disconnectButton)
+                    .addComponent(connectButton)
+                    .addComponent(usernameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 26, Short.MAX_VALUE)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 261, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(sendButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 69, Short.MAX_VALUE)))
+                    .addComponent(jScrollPane3))
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>                        
+
+    private void connectButtonActionPerformed(java.awt.event.ActionEvent evt) {                                              
+        // TODO add your handling code here:
+            if (isConnected == false) {
+//            username = usernameField.getText();
+            usernameField.setEditable(false);
+
+
+
+            try {
+//                sock = new Socket(serverIP, Port);
+                InputStreamReader streamreader = new InputStreamReader(sock.getInputStream());
+                reader = new BufferedReader(streamreader);
+                writer = new PrintWriter(sock.getOutputStream());
+//                writer.println(username + ":est connecté.:Connecté"); // Displays to everyone that user connected.
+                writer.flush(); // flushes the buffer
+                isConnected = true; // Used to see if the client is connected.
+            } catch (Exception ex) {
+                chatTextArea.append("Impossible de se connecter, réessayez !\n");
+                usernameField.setEditable(true);
+            }
+            ListenThread();
+        } else if (isConnected == true) {
+            chatTextArea.append("Vous êtes déjà connecté. \n");
+        }
+    }                                             
+
+    private void disconnectButtonActionPerformed(java.awt.event.ActionEvent evt) {                                                 
+        // TODO add your handling code here:
+//        sendDisconnect();
+        Disconnect();
+    }                                       
+    
+    
+    
+
+    private void sendButtonActionPerformed(java.awt.event.ActionEvent evt) {                                           
+        // TODO add your handling code here:
+        String nothing = "";
+        if ((inputTextArea.getText()).equals(nothing)) {
+            inputTextArea.setText("");
+            inputTextArea.requestFocus();
+        } else {
+            try {
+//               writer.println(username + ":" + inputTextArea.getText() + ":" + "Chat");
+               writer.flush(); // flushes the buffer
+            } catch (Exception ex) {
+                chatTextArea.append("Message non envoyé \n");
+            }
+            
+            inputTextArea.setText("");
+            inputTextArea.requestFocus();
+        }
+
+        inputTextArea.setText("");
+        inputTextArea.requestFocus();
+    }                                                                                         
 }
