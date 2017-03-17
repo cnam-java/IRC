d058bcb4239fec3f07b4fe314154afe7ea4d6243
GaëlCR <gaelcr@iMacGael.local>
2017-03-17 13:45:25 +0100

    Revert "[IRC] - MàJ fichier Window"
    This reverts commit 10843f4e1cfacc1b0b886eb8881f377b5f6d2eb6.

--- src/main/java/io/Window.java
+++ src/main/java/io/Window.java
@@ -1,36 +1,21 @@
 package io;
 
-import java.awt.Color; 
-import java.awt.FlowLayout;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
- 
-public class Window extends JFrame {
-	
-  /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
-public Window(){             
-    this.setTitle("Chat IRC Cnam");
-    this.setSize(800, 650);
-    this.setLocationRelativeTo(null);
-    
-    JButton bouton = new JButton("Envoyer !");
- 
-    //Instanciation d'un objet JPanel
-    JPanel pan = new JPanel();
-    pan.add(bouton);
-    pan.setLayout(new FlowLayout());
-    
-    //Définition de sa couleur de fond
-    pan.setBackground(Color.cyan);  
-   
-    //On prévient notre JFrame que notre JPanel sera son content pane
-    this.setContentPane(pan);               
-    this.setVisible(true);
-  }       
+public class Window {
+
+	public Window() {
+		JFrame window = new JFrame();
+		
+		
+		JButton bouton =new JButton("clique");
+		window.add(bouton);
+		window.setTitle("IRC");
+		window.setSize(400, 500);
+		window.setLocationRelativeTo(null);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+	}
+
 }
